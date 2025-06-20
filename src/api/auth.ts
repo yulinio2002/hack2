19d8928cdcb71cad471202b61019e8066129b872
Yulinio Zavala Mariño <yulinio.zavala@utec.edu.pe>
2025-06-19 23:40:13 -0500

    Refactor API module

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,10 +1,7 @@
-import axios from 'axios'
+import { api } from './client'
 
-// const API_BASE_URL = 'http://198.211.105.95:8080';
-const API_BASE_URL = 'http://localhost:8000';
+export const login = (data: { email: string; passwd: string }) =>
+  api.post('/authentication/login', data)
 
-export const login = (data: { email: string, passwd: string }) =>
-  axios.post(API_BASE_URL+'/authentication/login', data)
-
-export const register = (data: { email: string, passwd: string }) =>
-  axios.post(API_BASE_URL+'/authentication/register', data)
+export const register = (data: { email: string; passwd: string }) =>
+  api.post('/authentication/register', data)
